5e0fdf257a9641d47ec538ba8bf0fce56ba39e05
Rolando Antonio Mejía <50997752+rolandoaamejia@users.noreply.github.com>
2021-10-07 05:18:30 -0600

    menu responsive

--- js/index.js
+++ js/index.js
@@ -3,7 +3,25 @@ ________________________
 Navigation
 ________________________
 */
+const navOpen = documet.querySelector(".nav_hamburger");
+const navClose = documet.querySelector(".close_toggle");
+const menu = documet.querySelector(".nav_menu");
+const navContainer = documet.querySelector(".nav_menu");
 
+navOpen.addEventListener('click', ()=>{
+    menu.classList.add('open');
+    document.body.classList.add('active')
+    navContainer.style.left = "0";
+    navContainer.style.width = "30rem";
+});
+
+
+navClose.addEventListener('click', ()=>{
+    menu.classList.remove('open');
+    document.body.classList.remove('active')
+    navContainer.style.left = "-30rem";
+    navContainer.style.width = "0";
+});
 /*
 _________________________
 Pop Up
