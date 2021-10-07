3a1bbae0f9671cbe797a46dc2f08c15b5345870f
Rolando Antonio Mejía <50997752+rolandoaamejia@users.noreply.github.com>
2021-10-07 05:28:53 -0600

    arreglo de error en menu responsive

--- js/index.js
+++ js/index.js
@@ -8,17 +8,17 @@ const navClose = documet.querySelector(".close_toggle");
 const menu = documet.querySelector(".nav_menu");
 const navContainer = documet.querySelector(".nav_menu");
 
-navOpen.addEventListener('click', ()=>{
-    menu.classList.add('open');
-    document.body.classList.add('active')
+navOpen.addEventListener("click", () => {
+    menu.classList.add("open");
+    document.body.classList.add("active");
     navContainer.style.left = "0";
     navContainer.style.width = "30rem";
 });
 
 
-navClose.addEventListener('click', ()=>{
-    menu.classList.remove('open');
-    document.body.classList.remove('active')
+navClose.addEventListener("click", () => {
+    menu.classList.remove("open");
+    document.body.classList.remove("active");
     navContainer.style.left = "-30rem";
     navContainer.style.width = "0";
 });
