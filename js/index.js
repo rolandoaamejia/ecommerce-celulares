107c1f913bf3f1acea97a5f2ef362e4a1d5b2815
Rolando Antonio Mejía <50997752+rolandoaamejia@users.noreply.github.com>
2021-10-08 02:38:54 -0600

    fix indexjs

--- js/index.js
+++ js/index.js
@@ -1,41 +1,71 @@
 /*
-________________________
+=============
 Navigation
-________________________
-*/
-const navOpen = documet.querySelector(".nav_hamburger");
-const navClose = documet.querySelector(".close_toggle");
-const menu = documet.querySelector(".nav_menu");
-const navContainer = documet.querySelector(".nav_menu");
+=============
+ */
+const navOpen = document.querySelector(".nav__hamburger");
+const navClose = document.querySelector(".close__toggle");
+const menu = document.querySelector(".nav__menu");
+const scrollLink = document.querySelectorAll(".scroll-link");
+const navContainer = document.querySelector(".nav__menu");
 
 navOpen.addEventListener("click", () => {
-    menu.classList.add("open");
-    document.body.classList.add("active");
-    navContainer.style.left = "0";
-    navContainer.style.width = "30rem";
+  menu.classList.add("open");
+  document.body.classList.add("active");
+  navContainer.style.left = "0";
+  navContainer.style.width = "30rem";
 });
-
 
 navClose.addEventListener("click", () => {
-    menu.classList.remove("open");
-    document.body.classList.remove("active");
-    navContainer.style.left = "-30rem";
-    navContainer.style.width = "0";
+  menu.classList.remove("open");
+  document.body.classList.remove("active");
+  navContainer.style.left = "-30rem";
+  navContainer.style.width = "0";
 });
-/*
-_________________________
-Pop Up
-_________________________
-*/
 
 /*
-__________________________
+=============
+PopUp
+=============
+ */
+const popup = document.querySelector(".popup");
+const closePopup = document.querySelector(".popup__close");
+
+if (popup) {
+  closePopup.addEventListener("click", () => {
+    popup.classList.add("hide__popup");
+  });
+
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      popup.classList.remove("hide__popup");
+    }, 500);
+  });
+}
+
+/*
+=============
 Fixed Navigation
-__________________________
-*/
+=============
+ */
+
+const navBar = document.querySelector(".navigation");
+const gotoTop = document.querySelector(".goto-top");
 
 // Smooth Scroll
-// Fixed NavBar
+Array.from(scrollLink).map(link => {
+  link.addEventListener("click", e => {
+    // Prevent Default
+    e.preventDefault();
 
-//AOS
-AOS.init();
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    const navHeight = navBar.getBoundingClientRect().height;
+    const fixNav = navBar.classList.contains("fix__nav");
+    let position = element.offsetTop - navHeight;
+
+    if (!fixNav) {
+      position = position - navHeight;
+    }
+
+    
