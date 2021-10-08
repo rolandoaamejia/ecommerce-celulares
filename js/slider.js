d38e09511e1b8c800dccb7b7491ef787c1cf9579
Rolando Antonio Mejía <50997752+rolandoaamejia@users.noreply.github.com>
2021-10-08 03:39:48 -0600

    fin slider

--- js/slider.js
+++ js/slider.js
@@ -89,3 +89,27 @@ if (slider2) {
       animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
     }).mount();
   }
+  
+// News
+if (slider5) {
+    new Glide("#glide_5", {
+      type: "carousel",
+      startAt: 0,
+      perView: 3,
+      rewin: false,
+      autoplay: 3000,
+      animationDuration: 800,
+      animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
+      breakpoints: {
+        998: {
+          perView: 2,
+        },
+        768: {
+          perView: 1,
+        },
+      },
+    }).mount();
+  }
+  
+  AOS.init();
+  
