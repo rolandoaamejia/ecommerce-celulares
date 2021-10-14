14569a0c79a80b7a51523909e95dcbf11978819e
Rolando Antonio Mejía <50997752+rolandoaamejia@users.noreply.github.com>
2021-10-14 12:19:05 -0600

    fin

--- js/products.js
+++ js/products.js
@@ -1,30 +1,30 @@
 const getProducts = async () => {
-    try {
-      const results = await fetch("./data/products.json");
-      const data = await results.json();
-      const products = data.products;
-      return products;
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  
-  /*
-  =============
-  Load Category Products
-  =============
-   */
-  const categoryCenter = document.querySelector(".category__center");
-  
-  window.addEventListener("DOMContentLoaded", async function () {
-    const products = await getProducts();
-    displayProductItems(products);
-  });
-  
-  const displayProductItems = items => {
-    let displayProduct = items.map(
-      product => ` 
-      <div class="product category__products">
+  try {
+    const results = await fetch("./data/products.json");
+    const data = await results.json();
+    const products = data.products;
+    return products;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+/*
+=============
+Load Category Products
+=============
+ */
+const categoryCenter = document.querySelector(".category__center");
+
+window.addEventListener("DOMContentLoaded", async function () {
+  const products = await getProducts();
+  displayProductItems(products);
+});
+
+const displayProductItems = items => {
+  let displayProduct = items.map(
+    product => ` 
+                  <div class="product category__products">
                     <div class="product__header">
                       <img src=${product.image} alt="product">
                     </div>
